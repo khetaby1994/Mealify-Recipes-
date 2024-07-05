bee6ccc3b558ab93799daa778434f507fd5841ea
khetaby1994 <khetaby1994@gmail.com>
2024-07-05 06:55:38 +0300

    second commit

--- signin/js/signin.js
+++ signin/js/signin.js
@@ -6,28 +6,39 @@ var dataHelp=document.getElementById('dataHelp')
 
 
 var users=[]
+
+console.log(localStorage.getItem('users'));
+
 users=JSON.parse(localStorage.getItem('users'))
 
 var userdata
 // store userdata after validation
 submit.addEventListener('click',function () {
-    userdata={
-        signupEmail:signupEmail.value,
-        signupPassword:signupPassword.value,
-    };
+    
+if (localStorage.getItem('users')==null) {
+    dataHelp.classList.replace('d-none','d-block')
+} 
+else{
+        userdata={
+            signupEmail:signupEmail.value,
+            signupPassword:signupPassword.value,
+        };
 
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].signupEmail===userdata.signupEmail && users[i].signupPassword===userdata.signupPassword) {
-            window.location.replace('../fullversion/fullversion.html')
-           
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].signupEmail===userdata.signupEmail && users[i].signupPassword===userdata.signupPassword) {
+                window.location.replace('../fullversion/fullversion.html')
             
-        } else {
-            dataHelp.classList.replace('d-none','d-block')
-            
+                
+            } else {
+                dataHelp.classList.replace('d-none','d-block')
+                
+            }
+
+
         }
+}
 
 
-    }
     
 })
 
